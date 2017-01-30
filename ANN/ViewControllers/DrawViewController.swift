6db2e7c63437507c38ad5f33d3ed906704d9ff48
baic989 <baic989@gmail.com>
2017-01-30 12:22:35 +0100

    Draw VC added

---
+++ ANN/ViewControllers/DrawViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DrawViewController.swift
+//  ANN
+//
+//  Created by polaznik on 30/01/17.
+//  Copyright (c) 2017 Algebra. All rights reserved.
+//
+
+import UIKit
+
+class DrawViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
