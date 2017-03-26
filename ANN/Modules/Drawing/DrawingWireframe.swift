28a45c1c7e4f3f47fc2a1d6e8e9aa7aa6687c784
Hrvoje Baic <baic989@gmail.com>
2017-03-26 12:12:54 -0700

    base implementation for drawing wireframe

--- ANN/Modules/Drawing/DrawingWireframe.swift
+++ ANN/Modules/Drawing/DrawingWireframe.swift
@@ -6,4 +6,31 @@
 //  Copyright © 2017 Hrvoje Baic. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+final class DrawingWireframe: BaseWireframe {
+    
+    // MARK: - Module setup -
+    
+    func configureModule(with viewController: DrawingViewController) {
+        let interactor = DrawingInteractor()
+        let presenter = DrawingPresenter(wireframe: self, view: viewController, interactor: interactor)
+        viewController.presenter = presenter
+    }
+    
+    func show(with transition: Transition, animated: Bool = true) {
+        let moduleViewController = DrawingViewController()
+        configureModule(with: moduleViewController)
+        
+        show(moduleViewController, with: transition, animated: animated)
+    }
+}
+
+// MARK: - Extensions -
+
+extension DrawingWireframe: DrawingWireframeInterface {
+    
+    func navigate(to option: DrawingNavigationOption) {
+        
+    }
+}
